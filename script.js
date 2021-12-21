cb02950a3737d98d6fc62f9c6998f97d38fe5ed7
Manny Verma <mannyverma11@gmail.com>
2021-12-20 19:57:55 -0700

    adding game

---
+++ script.js
@@ -0,0 +1,13 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+  let randomNum = Math.floor(Math.random() * 9)+1;
+}
+return randomNum;
+
+const compareGuesses = (humanGuess, computerGuess, secretGuess) => {
+  
+}
